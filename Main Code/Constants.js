3a1bf2af6b9d90af00c2ff5635c8d092713a2764
Gaurang Shirsat <53374786+sgaurang19@users.noreply.github.com>
2021-08-07 23:45:36 +0530

    Delete Main Code directory

--- Main Code/Constants.js
+++
@@ -1,8 +0,0 @@
-const is_Absent = 0;
-const FULL_TIME_HR = 8;
-const PART_TIME_HR = 4;
-const WAGEPERHR = 20;
-const MONTH_DAYS = 20;
-const WORKING_HRS = 160;
-
-module.exports={is_Absent, FULL_TIME_HR, PART_TIME_HR, WAGEPERHR, MONTH_DAYS, WORKING_HRS };
